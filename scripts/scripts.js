b8507c4337dcf6e1e18d6103dcc8813303357a5c
lilneeks <nickodc96@gmail.com>
2022-03-21 00:14:09 -0700

    adding wager button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,18 @@
 // 1, 5 , 10, 25 , 50 , 100 , 1000
 const chips = [1,5,10,25,50,100,1000];
 console.log(chips);
+
+function giveChips(chips){
+
+}
+
+giveChips();
+
+
+
+
+
+
 // 52 cards
 const deck = [];
 
