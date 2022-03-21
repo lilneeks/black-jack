8c884a239e89bb3456a234cd9a0801bdaba9a860
lilneeks <nickodc96@gmail.com>
2022-03-21 05:38:39 -0700

    maybe loop is not needed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
-// 1, 5 , 10, 25 , 50 , 100 , 1000
-const chips = [1,5,10,25,50,100,1000];
+// maybe learn about objects
+// const chips = [1,5,10,25,50,100,1000];
 
-// populates user field if over max
+// populates user input field if user types over max value
 function imposeMinMax(el){
   if(el.value != ""){
     if(parseInt(el.value) < parseInt(el.min)){
@@ -13,19 +13,37 @@ function imposeMinMax(el){
   }
 }
 
-
+// add bank value will later add chips 
 function addBank(){
   var outputDisplay = document.getElementById('bankDisplay');
   var bankInput = document.getElementById('bankInput').value;
-  var a = +bankInput;
-  outputDisplay.innerHTML=a;
+  var display = +bankInput;
+  outputDisplay.innerHTML=display;
   }
    
 // need to generate an error if user tries to add bank that is not divisible by 5  
-function errorBank() {
-  
+
+const bank = [];
+// Maybe loop is not needed? try just if else statment 
+function bankError(num) {
+  for(var i = 0; i <= num; i++) {
+      if(i % 5 === 0)bank.push("Yes");
+      else bank.push(i)
+  }
+  return bank;
 }
 
+
+console.log(bankError(9));
+
+// $(document).ready(function() {
+//   $("#form").submit(function(event) {
+//     event.preventDefault();
+//     var userNumber = $("input#bankInput").val();
+//     var result = bankError(userNumber);
+//     $("#bankDisplay").text(result);
+//   });
+// });
 
 
 
